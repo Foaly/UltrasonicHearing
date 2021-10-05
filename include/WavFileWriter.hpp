c092803ca2da8f620887acdf922d14e35e51fd70
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2021-10-05 12:25:47 +0200

    Put code under the GPL 3.0 license.

--- include/WavFileWriter.hpp
+++ include/WavFileWriter.hpp
@@ -1,27 +1,24 @@
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 //
-// This code was inspired by the Recorder example of Paul Stoffregens Audio library
-// and the SFML SoundFileWriterWav class
-// Copyright (C) 2018 Maximilian Wagenbach (aka Foaly)
+// Ultrasonic Hearing - Enabling you to extend your senses into 
+// the ultrasonic range using a Teensy 4.1
+// Copyright (C) 2021  Maximilian Wagenbach
 //
-// This software is provided 'as-is', without any express or implied warranty.
-// In no event will the authors be held liable for any damages arising from the use of this software.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it freely,
-// subject to the following restrictions:
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// 1. The origin of this software must not be misrepresented;
-//    you must not claim that you wrote the original software.
-//    If you use this software in a product, an acknowledgment
-//    in the product documentation would be appreciated but is not required.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
-// 2. Altered source versions must be plainly marked as such,
-//    and must not be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source distribution.
-//
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
 
 #ifndef WAVFILEWRITER_INCLUDE
 #define WAVFILEWRITER_INCLUDE
