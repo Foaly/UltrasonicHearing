9a0396fcaa329e11a9bce1d075f6e42590f721bc
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2021-10-05 20:06:16 +0200

    Added first basic test.

--- README.md
+++ README.md
@@ -16,3 +16,16 @@ License
 
 All code is licensed under the GNU General Public License (GPL) version 3.0.
 For more information refer the file `LICENSE.txt`.
+
+
+Testing
+-------
+
+Unit tests of the pitch shifter are provided.
+They need to be run on the Teensy itself.
+With the Teensy connected run the following command:
+
+For Teensy 4.1 run: `pio test -e teensy41`
+
+For Teensy 3.6 run: `pio test -e teensy36`
+
---
+++ test/test_PitchShifter.cpp
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Ultrasonic Hearing - Enabling you to extend your senses into 
+// the ultrasonic range using a Teensy 4.1
+// Copyright (C) 2021  Maximilian Wagenbach
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//////////////////////////////////////////////////////////////////////
+
+
+#include <unity.h>
+
+#include <PitchShift.hpp>
+
+
+const int sampleRate = 192000;
+const int16_t semitones = -36;  // shift in semitones
+uint16_t failedTestCount = 0;
+
+
+void test_pitchShifter(void) {
+    PitchShift pitchShifter(sampleRate, semitones);
+
+    TEST_ASSERT_EQUAL(42, 42);
+}
+
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();
+    RUN_TEST(test_pitchShifter);
+    failedTestCount = UNITY_END();
+}
+
+
+void loop() {
+    if(failedTestCount > 0) {
+        // at least one test failed, blink fast
+        digitalWrite(13, HIGH);
+        delay(100);
+        digitalWrite(13, LOW);
+        delay(100);
+    }
+    else {
+        // all tests passed, blink slow
+        digitalWrite(13, HIGH);
+        delay(800);
+        digitalWrite(13, LOW);
+        delay(800);
+    }
+}
